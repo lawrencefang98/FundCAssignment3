ac46b9faa6e06a33d53aa2136066770eef01999b
manderoon <75654442+manderoon@users.noreply.github.com>
2021-10-18 17:05:39 +1100

    Update encryption.c
    Rewriting using private key encryption

--- encryption.c
+++ encryption.c
@@ -1,138 +1,205 @@
+/******************************************************************************
+ *                               HEADER FILES                                 *
+ ******************************************************************************/
+
 #include <stdio.h>
 #include <stdlib.h>
-
-#define MAX_FILE_NAME 30
-#define MAX_FILE_SIZE 50
-#define MAX_KEY_SIZE 50
-
-void encryptFile(char plaintext[], int ciphertext[], int key[]);
-void decryptFile(char decrypt[], int ciphertext[], int key[]);
-
+#include <string.h>
+#include "encryption.h"
 
 int main()
 {
-    char plaintext[MAX_FILE_SIZE];
-    int ciphertext[MAX_FILE_SIZE];
-    char decrypt[MAX_KEY_SIZE];
-    int choice;
 
-    int key[MAX_KEY_SIZE + 1] = {
-            18,	49,	46,	22,	43,	27,	27,	36,	36,	45,
-            41,	31,	7,	50,	38,	30,	2,	41,	6,	18,
-            31,	16,	22,	22,	38,	42,	19,	27,	44,	25,
-            31,	45,	50,	6,	26,	23,	32,	31,	38,	8,
-            28,	6,	5,	47,	22,	31,	33,	41,	19,	16
-    };
+    securityMenu();
 
-    printf("SECURITY MENU | 1 = Encrypt, 2 = Decrypt, 3 = Exit\n");
+    return 0;
+}
+
+void securityMenu()
+{
+    char input_b[BUF_SIZE];
+    char output_b[BUF_SIZE];
+    char key[MAX_PASS_LENGTH] = "password";
+    int choice = 0;
+
+    printSecurity();
+    printf("Enter your choice: ");
     scanf("%d", &choice);
 
-    while(1)
+    if(choice == 1)
     {
-        if(choice == 1)
-        {
-            encryptFile(plaintext, ciphertext, key);
-            printf("SECURITY MENU | 1 = Encrypt, 2 = Decrypt\n");
-            scanf("%d", &choice);
-        }
-        else if(choice == 2)
-        {
-            decryptFile(decrypt, ciphertext, key);
-            printf("SECURITY MENU | 1 = Encrypt, 2 = Decrypt\n");
-            scanf("%d", &choice);
-        }
-        
-        else
-            exit(1);
+        encryptMenu(input_b, output_b, key);
+
+        printSecurity();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
     }
-    
+
+    else if(choice == 2)
+    {
+        decryptMenu(input_b, output_b, key);
+
+        printSecurity();
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+    }
+
 }
 
-void encryptFile(char plaintext[], int ciphertext[], int key[])
+
+void printSecurity()
+{
+    printf("\n\n            SECURITY MENU             ");
+    printf("\n--------------------------------------\n");
+    printf("Choose from the following options: \n");
+    printf("1. Encrypt a file.\n2. Decrypt a file.\n");
+}
+
+
+void encryptMenu(char input_b[], char output_b[],
+                 char key[])
+{
+    char in_filename[MAX_FILE_NAME] = {0};
+    char out_filename[MAX_FILE_NAME] = {0};
+
+    printf("\n\n            ENCRYPTION MENU           \n");
+    printf("--------------------------------------\n");
+
+    printf("Enter the name of the plaintext file: ");
+    scanf("%s", in_filename);
+    printf("\n");
+
+    printf("Enter the name you want to give the encrypted file: ");
+    scanf("%s", out_filename);
+    printf("\n");
+
+    readFile(in_filename, input_b);
+    encryptText(input_b, output_b, key);
+    writeFile(in_filename, input_b, output_b);
+
+    printf("File encrypted successfully.\n");
+}
+
+void decryptMenu(char input_b[], char output_b[], char key[])
+{
+    char in_filename[MAX_FILE_NAME] = {0};
+    /*char out_filename[MAX_FILE_NAME] = {0};*/
+
+    printf("\n\n         DECRYPTION MENU          \n");
+    printf("--------------------------------------\n");
+
+    printf("Enter the name of the file you wish to decrypt: ");
+    scanf("%s", in_filename);
+    printf("\n");
+
+    /*printf("Enter the name you want to give the decrypted file: ");
+    scanf("%s", out_filename);
+    printf("\n");*/
+
+    readFile(in_filename, input_b);
+    encryptText(input_b, output_b, key);
+    writeFile(in_filename, input_b, output_b);
+
+    printf("File decrypted successfully.\n");
+}
+
+
+
+char readFile(char filename[], char input_b[])
 {
     FILE * file_p;
 
-    char filename[MAX_FILE_NAME];
-
-    printf("ENCRYPT | Enter the name of the file: ");
-    scanf("%s",filename);
+    int i;
 
     file_p = fopen(filename, "r");
 
     if(file_p == NULL)
     {
         printf("File not found.\n");
-        exit(0);
+        EXIT_FAILURE;
     }
 
-    else
+    if(!feof(file_p))
     {
-        int i = 0;
-        int j;
-
-        while(!feof(file_p))
+        for(i = 0; i < BUF_SIZE; i++)
         {
-            fscanf(file_p, "%c", &plaintext[i]);
-
-            ciphertext[i] = plaintext[i] ^ key[i];
-            i++;
+            fscanf(file_p, "%c", &input_b[i]);
         }
 
-        rewind(file_p);
+        printf("Text is: \n");
 
-        file_p = fopen(filename, "w");
-
-        for(j = 0; j < i; j++)
+        for(i = 0; i < 180; i++)
         {
-            fprintf(file_p, "%d", ciphertext[j]);
+            if (input_b[i] == '\0')
+                break;
+
+            printf("%c", input_b[i]);
+
         }
 
+        fclose(file_p);
+        printf("\n\n");
     }
 
-    fclose(file_p);
+    return input_b[BUF_SIZE];
 }
 
-void decryptFile(char decrypt[], int ciphertext[], int key[])
+void writeFile(char filename[], char input_b[], char output_b[])
 {
     FILE * file_p;
-    char filename[MAX_FILE_NAME];
 
-    printf("DECRYPT | Enter the name of the file: ");
-    scanf("%s",filename);
+    int i;
 
-    file_p = fopen(filename, "r");
+    file_p = fopen(filename, "w");
 
     if(file_p == NULL)
     {
         printf("File not found.\n");
-        exit(0);
+        EXIT_FAILURE;
     }
 
-    else
+    if(!feof(file_p))
     {
-        int i = 0;
-        int j;
-
-        while(!feof(file_p))
+        for(i = 0; i < BUF_SIZE; i++)
         {
-            fscanf(file_p, "%d", &ciphertext[i]);
-
-            decrypt[i] = ciphertext[i] ^ key[i];
-            i++;
+            if(i < strlen(input_b))
+            {
+                                    /* &input_b[i]); */
+                fprintf(file_p, "%s", &output_b[i]);
+            }
         }
 
-        rewind(file_p);
-
-        file_p = fopen(filename, "w");
-
-        for(j = 0; j < i; j++)
-        {
-            fprintf(file_p, "%d", ciphertext[j]);
-        }
-
-        printf("DECRYPT | File decrypted successfully.\n");
-
+        fclose(file_p);
     }
 
-    fclose(file_p);
+}
+
+char encryptText(char plaintext_b[], char ciphertext_b[], char key[])
+{
+    int i = 0;
+
+    while(plaintext_b[i] != '\0')
+    {
+        for(i = 0; i < strlen(plaintext_b); i++)
+        {
+            ciphertext_b[i] = plaintext_b[i] ^ key[i % strlen(key)];
+        }
+    }
+
+    return ciphertext_b[BUF_SIZE];
+}
+
+char decryptText(char plaintext_b[], char ciphertext_b[], char key[])
+{
+    int i = 0;
+
+    while(ciphertext_b[i] != '\0')
+    {
+        for(i = 0; i < strlen(plaintext_b); i++)
+        {
+            plaintext_b[i] = ciphertext_b[i] ^ key[i];
+        }
+    }
+
+    return ciphertext_b[BUF_SIZE];
 }
