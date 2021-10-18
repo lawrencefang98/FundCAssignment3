219ebc5bcf4c358ad8123839578e214eb9d332f6
AndLin-hub <13623905@student.uts.edu.au>
2021-10-18 21:30:58 +1100

    compression

--- newcompression.c
+++ newcompression.c
@@ -21,11 +21,34 @@ struct minNode* createNode(char symbol,int frequency){
     newNode->left = NULL;
     newNode->right = NULL;
     newNode->symbol = symbol;
+#include <stdio.h>
+#include <stdlib.h>
+
+struct minNode{
+    char symbol;
+    int frequency;
+    struct minNode* left, *right;
+};
+typedef struct minNode minNode;
+
+struct minHeap{
+    int currentSize;
+    int capacity;
+    struct minNode **array;
+};
+typedef struct minHeap minHeap;
+
+
+struct minNode* createNode(char symbol,int frequency){
+    struct minNode *newNode = (struct minNode *)malloc(sizeof(struct minNode));
+    newNode->left = NULL;
+    newNode->right = NULL;
+    newNode->symbol = symbol;
     newNode->frequency = frequency;
     return newNode;
 }
 
-struct minHeap createMinHeap(int capacity){
+struct minHeap* createMinHeap(int capacity){
     struct minHeap *minPriorityHeap = (struct minHeap *)malloc(sizeof(struct minHeap));
     minPriorityHeap->currentSize=0;
     minPriorityHeap->capacity = capacity;
@@ -33,15 +56,47 @@ struct minHeap createMinHeap(int capacity){
     return minPriorityHeap;
 }
 
-void insertInHeap(struct minHeap *minPriorityHeap, struct minNode *minHeapNode ){
+
+void orderHeap(struct minHeap *minPriorityHeap){
     int i;
-    minPriorityHeap->currentSize++;
-    while (i && minHeapNode->frequency < minPriorityHeap->array[(i - 1) / 2]->frequency) {
-    minPriorityHeap->array[i] = minPriorityHeap->array[(i - 1) / 2];
-    i = (i - 1) / 2;
+    int f;
+    for(i = 0; i < minPriorityHeap->currentSize; i ++){
+        for(f = 0; f < minPriorityHeap->currentSize; f ++){
+            if(minPriorityHeap->array[i]->frequency < minPriorityHeap->array[f]->frequency){
+                struct minNode* temp;
+                temp = minPriorityHeap->array[i];
+                minPriorityHeap->array[i] = minPriorityHeap->array[f];
+                minPriorityHeap->array[f] = temp;
+            }
+        }
     }
-    minPriorityHeap->array[i] = minHeapNode;
 }
+
+void insertInHeap(struct minHeap *minPriorityHeap, struct minNode *minHeapNode ){
+    minPriorityHeap->array[minPriorityHeap->currentSize] = minHeapNode;
+    minPriorityHeap->currentSize++;
+    orderHeap(minPriorityHeap);
+}
+
+int isOneSize(struct minHeap* minPriorityHeap){
+    if(minPriorityHeap->currentSize == 1){
+        return 1;
+    }
+    return 0;
+}
+
+struct minNode* extractMins(struct minHeap* minPriorityHeap){
+    struct minNode *newNode = (struct minNode *)malloc(sizeof(struct minNode));
+    newNode->left = minPriorityHeap->array[0];
+    newNode->right = minPriorityHeap->array[1];
+    newNode->frequency = minPriorityHeap->array[0]->frequency + minPriorityHeap->array[1]->frequency;
+    minPriorityHeap->array[0] = newNode;
+    minPriorityHeap->array[1] = minPriorityHeap->array[minPriorityHeap->currentSize - 1];
+    --minPriorityHeap->currentSize;
+    orderHeap(minPriorityHeap);
+    return newNode;
+}
+
 
 /*Gets the frequency of each character*/
 void getFrequency(char symbol[], int freq[]){
@@ -69,51 +124,34 @@ void getFrequency(char symbol[], int freq[]){
     fclose(fp);
 }
 
-void sortArray(char character[], int frequency[]){
-    char unSortedCharacter[256];
-    int unSortedFrequency[256];
-    int i;
-    int f;
-    int tempInt = 0;
-    char tempChar;
-
-    for(i = 0; i < 256; i++){
-        for(f = 1+i; f < 256; f++){
-            if(unSortedFrequency[i] > unSortedFrequency[f]){
-                tempInt = unSortedFrequency[i];
-                unSortedFrequency[i] = unSortedFrequency[f];
-                unSortedFrequency[f] = tempInt;
-                tempChar = unSortedCharacter[i];
-                unSortedCharacter[i] = unSortedCharacter[f];
-                unSortedCharacter[f] = tempChar;
-            }
-        }
-    }
-    for(i= 0; i <256; i++){
-        character[i] = unSortedCharacter[i];
-        frequency[i] = unSortedFrequency[i];
-    }
+void encode(struct minHeap *root){
+    
 }
 
-
 int main(){
-    struct minHeap minPriorityQ;
+    struct minHeap* minPriorityQ;
 	char character[256] = {0};
     int frequency[256] = {0};
     int i;
     int counter = 0;
     getFrequency(character,frequency);
-    sortArray(character, frequency);
     for(i = 0; i < 256; i++){
         if(frequency[i]> 0){
         counter++;
         }
     }
-    minPriorityQ = createMinHeap(counter);
+    minPriorityQ = createMinHeap(256);
     for(i = 0; i < 256; i++){
         if(frequency[i]> 0){
             insertInHeap(minPriorityQ,createNode(character[i],frequency[i]));
         }
     }
+    while(isOneSize(minPriorityQ) == 0){
+        extractMins(minPriorityQ);
+    }
+    for(i = 0; i < minPriorityQ->currentSize; i ++){
+        printf("%c%d ", minPriorityQ->array[i]->symbol,minPriorityQ->array[i]->frequency);
+    }
+    
     return 0;
 }
