09fb66f55c8d4e555f0804100f4e0438f4f9314b
manderoon <75654442+manderoon@users.noreply.github.com>
2021-10-22 17:28:55 +1100

    Update encryption.h

--- encryption.h
+++ encryption.h
@@ -3,74 +3,26 @@
  ******************************************************************************/
 
 #define MAX_FILE_NAME 30
-#define MAX_FILE_SIZE 1000
+#define MAX_FILE_SZ 1024
 #define LOGIN_DB "login_database.txt"
 
-#define MAX_PASS_LENGTH 20
-#define MIN_PASS_LENGTH 8
-
-#define MAX_ID_LENGTH 10
-#define MAX_CLINIC_SIZE 50
+#define MAX_PASS_LEN 20
+#define MIN_PASS_LEN 8
 
 #define BUF_SIZE 4096
-
-/* Define boolean data type */
-typedef int bool;
-#define TRUE 1
-#define FALSE 0
-
-/******************************************************************************
- *                                 STRUCTURES                                 *
- ******************************************************************************/
-
-typedef struct user {
-    int userID[MAX_ID_LENGTH + 1];
-    char password[MAX_PASS_LENGTH + 1];
-} user_t;
-
-/******************************************************************************
- *                      FUNCTION PROTOTYPES  |  PASSWORD                      *
- ******************************************************************************/
-
-/* User Menu */
-_Noreturn void loginMenu();
-void printMenu();
-
-/* Menu Options */
-void loginUser(user_t accounts[], int* total_p);
-void addUser(user_t accounts[], int* total_p);
-void saveUser(user_t accounts[], int* total_p);
-void deleteUser(user_t accounts[], int* total_p);
-
-/* Validity Check */
-bool checkChar(const char password[]);
-bool checkPass(int pass_buff[11], user_t accounts[], int* total_p);
-bool checkUser(int userID_buff[11], user_t accounts[], int* total_p);
-
-
 
 /******************************************************************************
  *                     FUNCTION PROTOTYPES  |  ENCRYPTION                     *
  ******************************************************************************/
 
 /* Menus */
-void securityMenu();
+_Noreturn void securityMenu();
 void printSecurity();
-void encryptMenu(char input_b[BUF_SIZE], char output_b[BUF_SIZE],
-                 char key[MAX_PASS_LENGTH]);
-void decryptMenu(char input_b[BUF_SIZE], char output_b[BUF_SIZE],
-                 char key[MAX_PASS_LENGTH]);
-
+void encryptMenu(char input_b[], char output_b[], char key[]);
+void decryptMenu(char input_b[], char output_b[], char key[]);
 
 /* Menu Options */
-char readFile(char filename[], char input_b[BUF_SIZE]);
-
-char encryptText(char plaintext_b[BUF_SIZE], char ciphertext_b[BUF_SIZE],
-                 char key[MAX_PASS_LENGTH]);
-
-char decryptText(char plaintext_b[BUF_SIZE], char ciphertext_b[BUF_SIZE],
-                 char key[MAX_PASS_LENGTH]);
-
-void writeFile(char filename[], char input_b[BUF_SIZE], 
-               char output_b[BUF_SIZE]);
+char readFile(char filename[], char input_b[]);
+void encryptDecrypt(char input_b[], char output_b[], char key[]);
+void writeFile(char filename[], char input_b[], char output_b[]);
 
