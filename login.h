27f687d63ec9c863b14c32297880deccb9172c2a
manderoon <75654442+manderoon@users.noreply.github.com>
2021-10-30 20:37:03 +1100

    Update login.h
    issues with saving user/pass to file

--- login.h
+++ login.h
@@ -53,7 +53,7 @@ void saveUser(user_t accounts[], int* total_p);
 
 /* Validity Check */
 int checkChar(const char password[]);
-int checkCharNum(const char username[]);
+int checkNum(const char username[]);
 
 /* Login Check */
 int validCred(const char credentials[]);
@@ -172,9 +172,9 @@ void addUser(user_t accounts[], int* total_p)
         }
 
         /* Debug: Username may only include numeric characters */
-        while(checkCharNum(accounts[i].username) == 0)
+        while(checkNum(accounts[i].username) == 0)
         {
-            printf("ERROR | Your ID can only include numbers.\n");
+            printf("ERROR | Your ID can only include numeric characters.\n");
             printf("\nADD USER | Enter your employee ID: ");
             scanf("%c", &space); /* Reads all text (including spaces) */
             scanf("%[^\n]", accounts[i].username);
@@ -286,9 +286,9 @@ void saveUser(user_t accounts[], int* total_p)
     {
         int i = 0;
 
-        while(i <= *total_p)
+        while(i <= *total_p - 1)
         {
-            fprintf(file_p, "%-8s %-20s\n",
+            fprintf(file_p, "%s %s\n",
                     accounts[i].username,
                     accounts[i].password);
             i++;
@@ -299,28 +299,18 @@ void saveUser(user_t accounts[], int* total_p)
 }
 
 
-int checkCharNum(const char username[])
+int checkNum(const char username[])
 {
-    int i = 0;
-    int j = 0;
+    int i;
+    int containsNum = 0;
 
-    /*for(i = 0; i <= ID_LEN + 1; ++i)*/
-    while(i <= ID_LEN + 1)
+    for(i = 0; i <= ID_LEN; i++)
     {
-        char ch = username[i++];
-
-        if(ch >= '0' && ch <= '9')
-            j++;
-
-        else
-            j = 0;
+        if(username[i] >= '0' && username[i] <= '9')
+            containsNum = 1;
     }
 
-    if(j > 0)
-        return 1;
-
-    else
-        return 0;
+    return containsNum;
 }
 
 
@@ -329,21 +319,17 @@ int checkChar(const char password[])
     int i;
     int containsChar = 0;
     /* String only contains alphanumeric characters */
-    for(i = 0; i <= MAX_PASS_LEN + 1; i++)
+    for(i = 0; i <= MAX_PASS_LEN; i++)
     {
-        char ch = password[i];
-
-        if ((ch <= 'A' && ch >= 'Z') ||
-            (ch <= 'a' && ch >= 'z') ||
-            (ch <= '0' && ch >= '9'))
+        if ((password[i] >= 'A' && password[i] <= 'Z') ||
+            (password[i] >= 'a' && password[i] <= 'z') ||
+            (password[i] >= '0' && password[i] <= '9'))
         {
             containsChar = 1;
         }
-
-
     }
-    return containsChar;
 
+    return containsChar;
 }
 
 
@@ -363,8 +349,7 @@ int validCred(const char credentials[])
 
     else
     {
-        /* https://stackoverflow.com/questions/20253177/how-to-count-how-many-times-a-word-appears-in-a-txt-file*/
-        while (fgets(text_b, MAX_FILE_SZ, file_p) != NULL)
+        while(fgets(text_b, MAX_FILE_SZ, file_p) != NULL)
         {
             char *ptr = text_b;
 
@@ -420,7 +405,7 @@ void loginUser(int* total_p)
             printf("\nERROR | Your password must not include spaces.\n");
         }
 
-        else if(checkCharNum(login.username) == 0)
+        else if(checkNum(login.username) == 0)
         {
             printf("\nERROR | Your ID must only include numeric characters.\n");
         }
