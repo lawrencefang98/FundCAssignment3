86eea5e2ca52cd1da2a9b1d24e7599c6cd864c3b
rosemoljohn <92021923+rosemoljohn@users.noreply.github.com>
2021-10-30 18:10:58 +1100

    Update main.c

--- main.c
+++ main.c
@@ -65,29 +65,37 @@ int main(void){
             case 6:
             /* read from database*/
             break; 
-
+                
             case 7:
+            /* delete from database*/
+            break;
+            
+             case 8:
+            /* merge */
+            break;
+
+            case 9:
             /* compress the file*/
                 compression();
             break; 
 
-            case 8:
+            case 10:
             /* Encrypt the file*/
                 loginMenu();
             break; 
             
-            case 79:
+            case 11:
             /* decrypt the file*/
             break; 
 
-            case 10:
+            case 12:
             exit(0);
             default:
                 check=1;
                 printf("Invalid choice.");
             break;
             }
-    }while (choice1 < 10 || choice1 > 1 || check == 0);
+    }while (choice1 < 12 || choice1 > 1 || check == 0);
     
             
         return 1;
@@ -98,12 +106,16 @@ void printMenu1(void){
     printf("\n\n"
          "1. For Doctor\n" 
          "2. For Admin\n"
-         "3. Save to Database\n"
-         "4. Read from Database\n"
-         "5. Compress the file\n"
-         "6. Encrypt the file\n"
-         "7. Decyrpt the file\n"
-         "8. Exit the program\n");
+         "3. Save doctor to Database\n"
+         "4. Save patient to Database\n"
+         "5. Save admin to Database\n" 
+         "6. Read from Database\n"
+         "7. delete file\n"
+         "8. Merge file\n"
+         "9. Compress the file\n"
+         "10. Encrypt the file\n"
+         "11. Decyrpt the file\n"
+         "12. Exit the program\n");
 }
 
 void printdoctor(){
